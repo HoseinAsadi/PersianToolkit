a29810cf55a7c408bfa63b6aac0a0ed0b31de525
Mahdi Hosseini <mahdidvb72@gmail.com>
2019-05-28 12:18:57 +0430

    some fix

--- PersianToolkit/Tools/HolidayHelper.cs
+++ PersianToolkit/Tools/HolidayHelper.cs
@@ -1,12 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Threading;
 
 namespace PersianToolkit
 {
@@ -25,39 +22,71 @@ namespace PersianToolkit
         public static readonly DependencyProperty DateProperty =
         DependencyProperty.RegisterAttached("Date", typeof(DateTime), typeof(HolidayHelper), new PropertyMetadata { PropertyChangedCallback = DatePropertyChanged });
 
-        private async static void DatePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void DatePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var date = GetDate(d);
-            SetIsHoliday(d, await CheckIsHoliday(date));
+            SetIsHoliday(d, CheckIsHoliday(date));
         }
 
-        private static async Task<bool> CheckIsHoliday(DateTime date)
+        private static bool CheckIsHoliday(DateTime date)
         {
-            bool result = false;
-            await Task.Run(() => {
-                PersianCalendar pc = new PersianCalendar();
+            PersianCalendar pc = new PersianCalendar();
 
-                //Todo: every year must be checked
-                // -1 is HijriAdjustment for fixing right day
-                var hijriNow = IslamicDateUtils.GregorianToIslamicDay(date.Year, date.Month, date.Day - 1);
-                JObject oo = JObject.Parse(Properties.Resources.events);
+            //Todo: every year must be checked
+            // -1 is HijriAdjustment for fixing right day
+            var hijriNow = IslamicDateUtils.GregorianToIslamicDay(date.Year, date.Month, date.Day - 1);
+            JObject oo = JObject.Parse(Properties.Resources.events);
 
-                var getPersianEvents = oo["Persian Calendar"].Where(x => x != null && x.SelectToken("day").ToString() == pc.GetDayOfMonth(date).ToString() &&
-                x.SelectToken("month").ToString() == pc.GetMonth(date).ToString() && x.SelectToken("type").ToString() == "Iran")
-                    .Select(m => (string)m.SelectToken("holiday")).ToArray();
+            var getPersianEvents = oo["Persian Calendar"].Where(x => x != null && x.SelectToken("day").ToString() == pc.GetDayOfMonth(date).ToString() &&
+            x.SelectToken("month").ToString() == pc.GetMonth(date).ToString() && x.SelectToken("type").ToString() == "Iran")
+                .Select(m => (string)m.SelectToken("holiday")).ToArray();
 
-                var getHijriEvents = oo["Hijri Calendar"].Where(x => x != null && x.SelectToken("day").ToString() == hijriNow.Day.ToString() &&
-                x.SelectToken("month").ToString() == hijriNow.Month.ToString() && x.SelectToken("type").ToString() == "Islamic Iran")
-                    .Select(m => (string)m.SelectToken("holiday")).ToArray();
+            var getHijriEvents = oo["Hijri Calendar"].Where(x => x != null && x.SelectToken("day").ToString() == hijriNow.Day.ToString() &&
+            x.SelectToken("month").ToString() == hijriNow.Month.ToString() && x.SelectToken("type").ToString() == "Islamic Iran")
+                .Select(m => (string)m.SelectToken("holiday")).ToArray();
 
 
-                if (string.Join(", ", getPersianEvents).Contains("True") || string.Join(", ", getHijriEvents).Contains("True"))
-                    result = true;
-                else
-                    result = false;
-            });
-            return result;
+            if (string.Join(", ", getPersianEvents).Contains("True") || string.Join(", ", getHijriEvents).Contains("True"))
+                return true;
+            else
+                return false;
         }
+
+        #region for Net 4.5 we can use async/await
+        //private async static void DatePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        //{
+        //    var date = GetDate(d);
+        //    SetIsHoliday(d, await CheckIsHoliday(date));
+        //}
+
+        //private static async Task<bool> CheckIsHoliday(DateTime date)
+        //{
+        //    bool result = false;            
+        //    await Task.Run(() => {
+        //        PersianCalendar pc = new PersianCalendar();
+
+        //        //Todo: every year must be checked
+        //        // -1 is HijriAdjustment for fixing right day
+        //        var hijriNow = IslamicDateUtils.GregorianToIslamicDay(date.Year, date.Month, date.Day - 1);
+        //        JObject oo = JObject.Parse(Properties.Resources.events);
+
+        //        var getPersianEvents = oo["Persian Calendar"].Where(x => x != null && x.SelectToken("day").ToString() == pc.GetDayOfMonth(date).ToString() &&
+        //        x.SelectToken("month").ToString() == pc.GetMonth(date).ToString() && x.SelectToken("type").ToString() == "Iran")
+        //            .Select(m => (string)m.SelectToken("holiday")).ToArray();
+
+        //        var getHijriEvents = oo["Hijri Calendar"].Where(x => x != null && x.SelectToken("day").ToString() == hijriNow.Day.ToString() &&
+        //        x.SelectToken("month").ToString() == hijriNow.Month.ToString() && x.SelectToken("type").ToString() == "Islamic Iran")
+        //            .Select(m => (string)m.SelectToken("holiday")).ToArray();
+
+
+        //        if (string.Join(", ", getPersianEvents).Contains("True") || string.Join(", ", getHijriEvents).Contains("True"))
+        //            result = true;
+        //        else
+        //            result = false;
+        //    });
+        //    return result;
+        //}
+        #endregion
 
         private static readonly DependencyPropertyKey IsHolidayPropertyKey =
         DependencyProperty.RegisterAttachedReadOnly("IsHoliday", typeof(bool), typeof(HolidayHelper), new PropertyMetadata());
